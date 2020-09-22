932ee3f8878181b2fd03250a58e825e96bcd7427
Bart Kuipers <bartkuipers88@gmail.com>
2020-09-22 16:11:08 +0200

    basic reqs

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,31 +1,24 @@
-import React from 'react';
-import actions from '../state/actions';
-import VideoThumbnail from './VideoThumbnail';
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-`;
+import React from "react";
+import actions from "../state/actions";
+import VideoThumbnail from "./VideoThumbnail";
 
 export default function VideoList({ dispatch, videos }) {
-    return (
-        <Wrapper>
-            {
-                videos.slice(0, 3).map((video, i) => {
-                    return (
-                        <VideoThumbnail
-                            dispatch={dispatch}
-                            key={`${video.name}-thumbnail`}
-                            name={video.name}
-                            onClick={() => {
-                                dispatch({ type: actions.SELECT_VIDEO, payload: i });
-                            }}
-                            src={video.thumbnail}
-                        />
-                    );
-                })
-            }
-        </Wrapper>
-    );
+  return (
+    <section className="video-list">
+      <h2>Up next...</h2>
+      {videos.map((video, i) => {
+        return (
+          <VideoThumbnail
+            dispatch={dispatch}
+            key={`${video.name}-thumbnail`}
+            name={video.name}
+            onClick={() => {
+              dispatch({ type: actions.SELECT_VIDEO, payload: i });
+            }}
+            src={video.thumbnail}
+          />
+        );
+      })}
+    </section>
+  );
 }
--- src/components/VideoRectangle.js
+++ src/components/VideoRectangle.js
@@ -1,46 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
-    Player,
-    ControlBar,
-    ReplayControl,
-    ForwardControl,
-    CurrentTimeDisplay,
-    TimeDivider,
-    PlaybackRateMenuButton,
-    VolumeMenuButton
-} from 'video-react';
-import HLSSource from './HLSSource';
-import _get from 'lodash/get';
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-    width: 50%;
-`;
+  Player,
+  ControlBar,
+  ReplayControl,
+  ForwardControl,
+  CurrentTimeDisplay,
+  TimeDivider,
+  PlaybackRateMenuButton,
+  VolumeMenuButton,
+} from "video-react";
+import HLSSource from "./HLSSource";
+import _get from "lodash/get";
 
 export default function VideoRectangle({ videoSelected, videos }) {
+  const [videoSource, setVideoSource] = useState(null);
 
-    const [videoSource, setVideoSource] = useState(null);
+  useEffect(() => {
+    setVideoSource(_get(videos, `${videoSelected}.path`, ""));
+  }, [videoSelected, videoSource, videos]);
 
-    useEffect(() => {
-        setVideoSource(_get(videos, `${videoSelected}.path`, ''));
-    }, [videoSelected, videoSource, videos]);
-
-    return (
-        <Wrapper>
-            <Player>
-                <HLSSource
-                    isVideoChild
-                    src={videoSource}
-                />
-                <ControlBar>
-                    <ReplayControl seconds={10} order={1.1} />
-                    <ForwardControl seconds={30} order={1.2} />
-                    <CurrentTimeDisplay order={4.1} />
-                    <TimeDivider order={4.2} />
-                    <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
-                    <VolumeMenuButton disabled />
-                </ControlBar>
-            </Player>
-        </Wrapper>
-    );
+  return (
+    <section className="player-wrapper">
+      <Player autoPlay>
+        <HLSSource isVideoChild src={videoSource} />
+        <ControlBar>
+          <ReplayControl seconds={10} order={1.1} />
+          <ForwardControl seconds={30} order={1.2} />
+          <CurrentTimeDisplay order={4.1} />
+          <TimeDivider order={4.2} />
+          <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
+          <VolumeMenuButton disabled />
+        </ControlBar>
+      </Player>
+    </section>
+  );
 }
